970c9008d4fcd10146861d7306345c12142719dd
Jayasruthy Karthikeyan <jayasruthyk6@gmail.com>
2025-08-11 18:51:08 +0530

    Initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    {/*for tailwind setup verification */}
+    <p className="text-red-500">Text for Demo</p>
+    </>
+  )
+}
+
+export default App
